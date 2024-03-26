b66bf19d15ddc432320d1cb5dbecda40089420c3
nechip-nik <iv123s18>
2024-03-26 17:28:49 +0700

    add lab2

---
+++ semester_6/CT/lab2/CoolLexer.h
@@ -0,0 +1,26 @@
+#ifndef COOLLEXER_H
+#define COOLLEXER_H
+
+#include <iostream>
+#include <fstream>
+
+#undef yyFlexLexer
+#include <FlexLexer.h>
+#include <vector>
+
+class CoolLexer : public yyFlexLexer {
+public:
+    CoolLexer(std::istream& arg_yyin, std::ostream& arg_yyout) :
+        yyFlexLexer{arg_yyin, arg_yyout}, out{arg_yyout}, lineno{0}, comment_level{0} {}
+    virtual int yylex();
+
+private:
+    void Error(const char* msg) const;
+    void Escape();
+
+    std::ostream& out;
+    int lineno;
+    int comment_level;
+};
+
+#endif
